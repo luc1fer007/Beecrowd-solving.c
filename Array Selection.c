f2050f9bcde555e2f1b6537bf44bf1ac38d9d1e1
luc1fer007 <arifulhassanjoy02@gmail.com>
2025-06-12 15:16:26 +0600

    Create Array Selection.c

---
+++ Array Selection.c
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;double A[100];
+    for(i = 0 ; i < 100 ; i++)
+    {
+        scanf("%lf",&A[i]);
+    }
+    for(i = 0 ; i < 100 ; i++)
+    {
+        if(A[i] <= 10)
+        {
+            printf("A[%d] = %.1lf\n",i,A[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return 0;
+
+}
